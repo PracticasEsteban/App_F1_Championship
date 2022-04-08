e9745592ada58612511f69ce3b5209ba3e7e7224
EstebanJPG <esteban.jimenez.piquerass@gmail.com>
2022-04-08 15:06:53 +0200

    Empezamos Stencil

--- Stencil/table/src/components/my-component/my-component.tsx
+++ Stencil/table/src/components/my-component/my-component.tsx
@@ -1,5 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component,State, h, Watch } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
@@ -7,26 +6,77 @@ import { format } from '../../utils/utils';
   shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
+    
+  @State() clasificacion:any=[];
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
+  @State() seleccionadoOption:any;
+  @State() racesArray=[];
 
-  /**
-   * The last name
-   */
-  @Prop() last: string;
+  componentDidLoad(){
+    console.log('Entro')
 
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
+    this.getRaces();
+    this.seleccionadoOption=this.racesArray[0];
+    this.consultarDatos();
   }
+
+  @Watch('consultar')
+  consultarDatos():void{
+
+    if(this.seleccionadoOption==="Global"){
+  
+      this.getGlobalRank().then;
+    }else{
+      this.getRankingCarrera(this.seleccionadoOption);
+    }
+  }
+  
+  handleSecondSelect(event) {
+    console.log(event.target.value);
+    this.seleccionadoOption= event.target.value;
+  }
+
+  async getRaces(){
+    let response = await fetch('http://localhost:9000/races');
+    this.racesArray = await response.json();
+    if(this.seleccionadoOption==null){
+      this.seleccionadoOption=this.racesArray[0];
+    }
+  }
+  async getGlobalRank() {
+    
+  let response = await fetch('http://localhost:9000/ranking');
+  this.clasificacion = await response.json();
+ 
+  
+ }
+
+ async getRankingCarrera(carrera:string) {
+    
+  let response = await fetch("http://localhost:9000/ranking/carrera?carrera="+carrera);
+  this.clasificacion = await response.json();
+  
+ }
+ 
+
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>
+      <div class="positionRanking">
+      <h5><i>Ranking / {this.seleccionadoOption}</i></h5>
+
+      <select name="miselect" class="button-right" onInput={(event) => this.handleSecondSelect(event)}>
+        <option value="" disabled selected hidden>Elige la clasificacion</option>
+        {this.racesArray.map((race:any) =>
+          <option value={race} selected={this.seleccionadoOption === {race}}>{race}</option>
+        )}
+      </select>
+    
+      </div>
+
+    </div>;
   }
+
+
+  
 }
